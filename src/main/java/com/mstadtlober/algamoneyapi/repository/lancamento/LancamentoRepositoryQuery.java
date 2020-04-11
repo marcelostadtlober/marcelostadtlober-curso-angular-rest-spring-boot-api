2845db81e4da477cf90adb7dbef04a09b31bb2dd
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-04-10 23:44:24 -0300

    Implementando a paginação de lançamentos

--- src/main/java/com/mstadtlober/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/mstadtlober/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package com.mstadtlober.algamoneyapi.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.mstadtlober.algamoneyapi.model.Lancamento;
 import com.mstadtlober.algamoneyapi.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 	
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 }
