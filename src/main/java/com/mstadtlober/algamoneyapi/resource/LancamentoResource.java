c8fefb8526ef81e01e033e66fd6d549abd6b2b5d
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-04-10 23:12:20 -0300

    Desafio: Removendo lançamentos

--- src/main/java/com/mstadtlober/algamoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/mstadtlober/algamoneyapi/resource/LancamentoResource.java
@@ -12,12 +12,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mstadtlober.algamoneyapi.event.RecursoCriadoEvent;
@@ -54,6 +56,12 @@ public class LancamentoResource {
 		return this.lancamentoRepository.findById(codigo)
 				.map(lancamento -> ResponseEntity.ok(lancamento))
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover (@PathVariable Long codigo) {
+		this.lancamentoRepository.deleteById(codigo);
 	}
 	
 	@PostMapping
