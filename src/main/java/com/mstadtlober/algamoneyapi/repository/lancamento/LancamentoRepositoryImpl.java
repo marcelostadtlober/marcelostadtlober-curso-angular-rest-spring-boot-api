17ff54574f78d7e47ff5631cec9bd5cfd8598346
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-04-16 22:48:26 -0300

    Implementando projeção de lançamento

--- src/main/java/com/mstadtlober/algamoneyapi/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/mstadtlober/algamoneyapi/repository/lancamento/LancamentoRepositoryImpl.java
@@ -16,9 +16,12 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
+import com.mstadtlober.algamoneyapi.model.Categoria_;
 import com.mstadtlober.algamoneyapi.model.Lancamento;
 import com.mstadtlober.algamoneyapi.model.Lancamento_;
+import com.mstadtlober.algamoneyapi.model.Pessoa_;
 import com.mstadtlober.algamoneyapi.repository.filter.LancamentoFilter;
+import com.mstadtlober.algamoneyapi.repository.projection.ResumoLancamento;
 
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 	
@@ -39,9 +42,28 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		
 		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
 	}
-
-
-
+	
+	@Override
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<ResumoLancamento> criteria = builder.createQuery(ResumoLancamento.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class);
+		
+		criteria.select(builder.construct(ResumoLancamento.class
+					, root.get(Lancamento_.CODIGO), root.get(Lancamento_.DESCRICAO)
+					, root.get(Lancamento_.DATA_VENCIMENTO), root.get(Lancamento_.DATA_PAGAMENTO)
+					, root.get(Lancamento_.VALOR), root.get(Lancamento_.TIPO)
+					, root.get(Lancamento_.CATEGORIA).get(Categoria_.NOME)
+					, root.get(Lancamento_.PESSOA).get(Pessoa_.NOME)));
+		
+		Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+		criteria.where(predicates);
+		
+		TypedQuery<ResumoLancamento> query = manager.createQuery(criteria);
+		adicionarRestricoesDePaginacao(query, pageable);
+		
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
+	}
 
 	private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
 			Root<Lancamento> root) {
@@ -66,7 +88,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		return predicates.toArray(new Predicate[predicates.size()]);
 	}
 
-	private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+	private void adicionarRestricoesDePaginacao(TypedQuery<?> query, Pageable pageable) {
 		int paginaAtual = pageable.getPageNumber();
 		int totalRegistrosPorPagina = pageable.getPageSize();
 		int primeiroRegistroDaPagina = paginaAtual * totalRegistrosPorPagina;
--- src/main/java/com/mstadtlober/algamoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/mstadtlober/algamoneyapi/resource/LancamentoResource.java
@@ -30,6 +30,7 @@ import com.mstadtlober.algamoneyapi.exceptionhandler.AlgamoneyExceptionHandler.E
 import com.mstadtlober.algamoneyapi.model.Lancamento;
 import com.mstadtlober.algamoneyapi.repository.LancamentoRepository;
 import com.mstadtlober.algamoneyapi.repository.filter.LancamentoFilter;
+import com.mstadtlober.algamoneyapi.repository.projection.ResumoLancamento;
 import com.mstadtlober.algamoneyapi.service.LancamentoService;
 import com.mstadtlober.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
 
@@ -53,6 +54,12 @@ public class LancamentoResource {
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+	}
+	
+	@GetMapping(params = "resumo")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return lancamentoRepository.resumir(lancamentoFilter, pageable);
 	}
 	
 	@GetMapping("/{codigo}")
